7557a7b6217936ff4e3c17303ec67e3b3d4d7bc4
Cuong Liam <cuongdeptrai2706@gmail.com>
2026-04-22 21:16:14 +0700

    ex1

---
+++ src/main/java/com/restaurant/bai1/config/AppInit.java
@@ -0,0 +1,31 @@
+package com.restaurant.bai1.config;
+
+import jakarta.servlet.Filter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class}; // ✅ sửa ở đây
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"}; // bắt tất cả request
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("UTF-8");
+        encodingFilter.setForceEncoding(true);
+        return new Filter[]{encodingFilter};
+    }
+}
